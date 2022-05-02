71556db8012f2d08028c0e74691cd7c5ca14aadd
Luciano Borba <luhborbafilho@gmail.com>
2022-05-02 10:30:57 -0300

    Update Portifolio.py

--- Portifolio.py
+++ Portifolio.py
@@ -8,9 +8,7 @@ import Paginas.pagina_inicial as pi
 import warnings
 warnings.filterwarnings('ignore')
 
-st.markdown('<style>body{
-background-image:url(../image/dad.jpg);
-}</style>')
+st.markdown('<style>body{background-image:url(../image/dad.jpg);}</style>')
 
 #Titulo
 st.title('Luciano Borba - Projetos')
